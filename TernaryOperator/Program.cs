8d3ebd6d45a5c2ce93fa57d6f9b03369828d1d42
michaelbuchanan23 <michaelbuchanan23@gmail.com>
2018-06-01 12:56:09 -0400

    updated comments

--- TernaryOperator/Program.cs
+++ TernaryOperator/Program.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 namespace TernaryOperator {
 	class Program {
 		static void Main(string[] args) {
+
+			//note that CODE SECTION 1 below can be rewritten as CODE SECTION 2 below
+
+			//CODE SECTION 1 BEGIN
+			int n = 3;
+			string s;
+
+			if (n % 2 == 0) {
+				s = "even";
+			} else {
+				s = "odd";
+			//CODE SECTION 1 END
+
+			//CODE SECTION 2 BEGIN
+			int n = 3;
+			string s;
+
+			s = (n % 2 == 0) ? "even" : "odd"; //this is useful (for example) to check if a value was updated correctly
+			  //CODE SECTION 2 END
+
+			}
 		}
-	}
 }
